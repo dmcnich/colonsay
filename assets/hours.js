626235b3c4a0bf34f6821f4365b73cee097ad5a4
Duncan McNicholl <d@mcni.ch>
2017-10-19 18:34:58 +0100

    Changed images to CC photos

--- assets/hours.js
+++ assets/hours.js
@@ -2,36 +2,27 @@ var currentTime = new Date().getHours();
 var pre = "url(https://dmcnich.github.io/colonsay/images/"
 var post = ".jpeg) 50% top / auto 100vh no-repeat fixed rebeccapurple"
 
-if (7 <= currentTime && currentTime < 9) {
+
+else if (7 <= currentTime && currentTime < 11) {
+    document.getElementById("target").style.background = pre+"logs"+post
+    }
+
+else if (11 <= currentTime && currentTime < 15) {
+    document.getElementById("target").style.background = pre+"sawmill"+post
+    }
+
+else if (15 <= currentTime && currentTime < 19) {
     document.getElementById("target").style.background = pre+"kiloran"+post
     }
-else if (9 <= currentTime && currentTime < 11) {
-    document.getElementById("target").style.background = pre+"paps"+post
+
+else if (19 <= currentTime && currentTime < 23) {
+    document.getElementById("target").style.background = pre+"sunset"+post
     }
-else if (11 <= currentTime && currentTime < 13) {
-    document.getElementById("target").style.background = pre+"aerial"+post
+
+else if (currentTime < 3 || 23 <= currentTime ) {
+    document.getElementById("target").style.background = pre+"kilorain"+post
     }
-else if (13 <= currentTime && currentTime < 15) {
-    document.getElementById("target").style.background = pre+"green"+post
-    }
-else if (15 <= currentTime && currentTime < 17) {
-    document.getElementById("target").style.background = pre+"harbour"+post
-    }
-else if (17 <= currentTime && currentTime < 19) {
-    document.getElementById("target").style.background = pre+"high"+post
-    }
-else if (19 <= currentTime && currentTime < 21) {
-    document.getElementById("target").style.background = pre+"scalasaig"+post
-    }
-else if (21 <= currentTime && currentTime < 23) {
-    document.getElementById("target").style.background = pre+"seil"+post
-    }
-else if (23 <= currentTime) {
-    document.getElementById("target").style.background = pre+"south"+post
-    }
-else if (1 <= currentTime && currentTime < 3) {
-    document.getElementById("target").style.background = pre+"stone"+post
-    }
-else if (3 <= currentTime && currentTime < 5) {
-    document.getElementById("target").style.background = pre+"sunkiss"+post
+
+else if (3 <= currentTime && currentTime < 7) {
+    document.getElementById("target").style.background = pre+"garden"+post
     }
--- gallery.md
+++ gallery.md
@@ -2,30 +2,19 @@
 title: The View
 layout: default
 images:
-- path: aerial
-  title: From the air
-- path: green
-  title: Rock pool
-- path: harbour
-  title: Scalasaig harbour
-- path: high
-  title: Kiloran from on high
 - path: kiloran
-  title: Kiloran from lower down
-- path: paps
-  title: The paps of Jura
-- path: scalasaig
-  title: The village
-- path: seil
-  title: The view from oronsay
-- path: south
-  title: Kiloran from the north
-- path: stone
-  title: Fingal's limpet hammer
-- path: sunkiss
-  title: A sunset from somewhere
+  title: Kiloran in the sun
+- path: logs
+  title: Fresh sawn wood
 - path: sunset
   title: A sunset at Port Mor
+- path: kilorain
+  title: Kiloran not in the sun
+- path: sawmill
+  title: The abandoned estate sawmill
+- path: garden
+  title: The community garden
+
 ---
 
 <ul class="gallery">
